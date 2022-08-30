578e33d790501763e37ce2827f258e403df308a5
Azwar Tri Endrayadi <azwartrie123@gmail.com>
2022-08-30 16:29:56 +0700

    UI Dashboard Peta

--- src/app/modules/dashboard/components/Peta-Kejadian.tsx
+++ src/app/modules/dashboard/components/Peta-Kejadian.tsx
@@ -1,120 +1,38 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {FC} from 'react'
+import '../Layout.css'
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
-const PetaKejadian: FC = () => {
+function PetaKejadian() {
   return (
     <div className='card card-flush'>
       <div className='card-body'>
         <div className='tab-content' id='myTabContent'>
           <div className='tab-pane fade show active' id='kt_tab_pane_1' role='tabpanel'>
             <div className='row'>
-              <div className='col-md-6 col-lg-6 col-sm-12 mb-4'>
+              <div className='col-md-12 col-lg-12 col-sm-12 mb-4'>
                 <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Per Wilayah
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieW chartID='pie-one' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan Status Kepegawaian (PNS, PTT, PJLP)
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieK chartID='pie-two' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12 mb-4'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan Tingkat Pendidikan Terakhir (SMA, S1,
-                      S2, S3)
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieTPT chartID='pie-three' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan Golongan
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieG chartID='pie-four' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12 mb-4'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan Jenis Eselon
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieJE chartID='pie-five' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan Jenis Kediklatan
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieJK chartID='pie-six' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12 mb-4'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan Usia
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieU chartID='pie-seven' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan PPNS
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PiePPNS chartID='pie-eight' />
-                  </div> */}
-                </div>
-              </div>
-              <div className='col-md-6 col-lg-6 col-sm-12'>
-                <div className='card card-bordered'>
-                  {/* <div className='card-header justify-content-center'>
-                    <h3 className='card-title text-center'>
-                      Jumlah Personil Satpol PP Berdasarkan Usia Pensiun
-                    </h3>
-                  </div>
-                  <div className='card-body'>
-                    <PieUP chartID='pie-nine' />
-                  </div> */}
+                  <MapContainer center={[-6.183216, 106.830378]} zoom={50} scrollWheelZoom={true}>
+                    <TileLayer
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                      url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    />
+                    <Marker position={[-6.2, 106.816666]}>
+                      <Popup>
+                        Peta Ploting <br /> Anggota
+                      </Popup>
+                    </Marker>
+                    <Marker position={[-6.183339, 106.830137]}>
+                      <Popup>
+                        Peta Ploting <br /> Anggota2
+                      </Popup>
+                    </Marker>
+                    <Marker position={[-6.182993, 106.830782]}>
+                      <Popup>
+                        Peta Ploting <br /> Anggota3
+                      </Popup>
+                    </Marker>
+                  </MapContainer>
                 </div>
               </div>
             </div>
