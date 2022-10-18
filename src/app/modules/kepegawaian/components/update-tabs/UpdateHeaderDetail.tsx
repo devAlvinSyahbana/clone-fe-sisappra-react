bb78b8dd1eadcc98fac38568d9c6f61454bcee2e
Alvin Syahbana <dev.alvin.syahbana@gmail.com>
2022-10-18 16:33:07 +0700

    fix doEditFoto

--- src/app/modules/kepegawaian/components/update-tabs/UpdateHeaderDetail.tsx
+++ src/app/modules/kepegawaian/components/update-tabs/UpdateHeaderDetail.tsx
@@ -148,6 +148,9 @@ const UpdateHeaderDetail = () => {
     setSelectedFile({croppedImage: null})
     setImgSrc('')
     setCrop(undefined)
+    setRotate(0)
+    setScale(1)
+    setAspect(undefined)
   }
 
   const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
