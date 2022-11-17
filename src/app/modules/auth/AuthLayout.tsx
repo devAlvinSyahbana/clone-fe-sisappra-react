728671563dd8c90211784bb056ed249beeec574f
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-11-17 15:03:46 +0700

    edit authlayout tambah responsive

--- src/app/modules/auth/AuthLayout.tsx
+++ src/app/modules/auth/AuthLayout.tsx
@@ -17,13 +17,21 @@ const AuthLayout = () => {
   }, [])
 
   return (
-    <div className='d-flex flex-column flex-lg-row flex-column-fluid h-100'>
+    <div
+      className='d-flex flex-column flex-lg-row flex-column-fluid h-100'
+      style={{
+        // backgroundColor: '#151521',
+        opacity: '1',
+        background:
+          'repeating-linear-gradient( -45deg, #1a1a1f, #1a1a1f 5px, #151521 5px, #151521 25px',
+      }}
+    >
       {/* begin::Body */}
       <div className='d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1'>
         {/* begin::Form */}
         <div className='d-flex flex-center flex-column flex-lg-row-fluid'>
           {/* begin::Wrapper */}
-          <div className='d-flex flex-column flex-center py-5 px-5 px-md-15 w-100'>
+          <div className='d-flex flex-column flex-center py-5 px-5 px-md-15 mt-n15 mt-lg-0 w-100'>
             <Outlet />
           </div>
           {/* end::Wrapper */}
@@ -36,32 +44,18 @@ const AuthLayout = () => {
       <div className='d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2'>
         {/* begin::Content */}
         <div className='d-flex flex-column flex-center pb-5 pt-10 px-5 py-md-15 px-md-15 w-100'>
-          {/* begin::Logo */}
-          {/* <Link to='/' className='mb-12'>
-            <img alt='Logo' src={toAbsoluteUrl('/media/logos/custom-1.png')} className='h-75px' />
-          </Link> */}
-          {/* end::Logo */}
-
-          {/* begin::Image */}
-          {/* <img
-            className='mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20'
-            src={toAbsoluteUrl('/media/misc/auth-screens.png')}
-            alt=''
-          /> */}
-          {/* end::Image */}
-
           {/* begin::Title */}
-          <h1 className='text-white fs-5x fw-bold text-center mb-7'>SISAPPRA</h1>
+          <h1 className='text-white fs-5x fw-bold text-center mb-4'>SISAPPRA</h1>
           {/* end::Title */}
 
           {/* begin::Text */}
-          <div className='text-white fs-1 text-center'>
+          <div className='text-white fs-1 text-center mb-2'>
             Sistem Informasi Satuan Polisi Pamong Praja
           </div>
           <img
             alt='Logo'
             src={toAbsoluteUrl('/myasset/logosatpol.png')}
-            className='h-150px h-lg-350px'
+            className='h-150px h-md-225px h-lg-350px'
           />
           {/* end::Text */}
         </div>
