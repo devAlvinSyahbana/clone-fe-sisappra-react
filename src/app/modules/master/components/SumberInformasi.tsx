3ff3e2ba5b2af52386360f5a52a823568cef2365
fifs17 <fifs17@users.noreply.github.com>
2022-11-28 23:11:05 +0700

    Perbaikan Nomor Urut2

--- src/app/modules/master/components/SumberInformasi.tsx
+++ src/app/modules/master/components/SumberInformasi.tsx
@@ -181,7 +181,7 @@ export function SumberInformasi() {
 
   const [data, setData] = useState([])
   const [temp, setTemp] = useState<any[]>([])
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [qParamFind, setUriFind] = useState({strparam: ''})
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
@@ -302,13 +302,17 @@ export function SumberInformasi() {
     fetchDT(1)
   }, [qParamFind, perPage])
 
-  const fetchUsers = async (page: any) => {
+  const fetchUsers = async (page: any) => { //urutan 3
     setLoading(true)
     const value = await axios.get(`${SUMBER_INFORMASI_URL}/find`)
-    let items = value.data.data 
+    const timeout = setTimeout(() => {
+      let items = value.data.data
     Array.from(items).forEach((item: any, index: any) => {
-       item.serial = index + 1})
+      item.serial = index + 1
+    })
     setTemp(items)
+    }, 150);
+    return () => clearTimeout(timeout)
   }
   // END :: VIEW
   const handleChangeFormik = (event: {
@@ -547,7 +551,7 @@ export function SumberInformasi() {
           <DataTable
           columns={columns}
           data={temp}
-          // progressPending={loading}
+          progressPending={loading}
           customStyles={customStyles}
           progressComponent={<LoadingAnimation />}
           pagination
