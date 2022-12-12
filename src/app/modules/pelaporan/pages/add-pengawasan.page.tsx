56327a16b27b4e9178aae855ee555cf7b9bd75e2
Azwar Tri Endrayadi <azwartrie123@gmail.com>
2022-12-12 16:09:09 +0700

    add

---
+++ src/app/modules/pelaporan/pages/add-pengawasan.page.tsx
@@ -0,0 +1,250 @@
+import React, {FC, useEffect, useState, FormEvent, useRef} from 'react'
+import {StepDetailKegiatan} from './steps/step-detail-kegiatan'
+import {StepTindaklanjut} from './steps/step-tindaklanjut'
+import {StepDokumentasi} from './steps/step-dokumentasi'
+import {useDispatch, useSelector} from 'react-redux'
+import {
+  createSchemaPelaporanKegiatan,
+  initialState,
+  PelaporanKegiatanState,
+  changedValue,
+  isApelRapat,
+} from '../../../redux/slices/pelaporan-kegiatan.slice'
+import {Formik, Form, FormikValues, FormikContext} from 'formik'
+import axios from 'axios'
+import {ToFieldStateBNV} from '../components/fields.formikcto'
+import {RootState} from '../../../redux/store'
+import useMultistepForm from './steps/useMultistepForm'
+
+const excludeJenisKegiatan = [
+  'SIDANG TIPIRING',
+  'PENERTIBAN BANGUNAN',
+  'KEGIATAN PPKM',
+  'LAPORAN MASYARAKAT',
+  'PENERTIBAN MINUMAN BERALKOHOL',
+  'PENGAMANAN',
+]
+
+export const AddPengawasanPage: FC = () => {
+  const [currentSchema, setCurrentSchema] = useState(createSchemaPelaporanKegiatan[0])
+  const [val, setVal] = useState<any>(initialState)
+
+  const dispatch = useDispatch()
+  const jenisKegiatanId = useSelector(
+    (s: RootState) => s.pelaporanKegiatan.kegiatan__jenis_kegiatan_id
+  )
+
+  const {steps, currentStepIndex, step, isFirstStep, isLastStep, back, next} = useMultistepForm([
+    <StepDetailKegiatan values={val} setVal={setVal} />,
+    ...(isApelRapat(val) ? [<StepDokumentasi />] : [<StepTindaklanjut />, <StepDokumentasi />]),
+  ])
+
+  const updateJenisKegiatanList = () => {
+    axios.get(`http://localhost:3001/jenis-kegiatan/combobox?$orderby=nama`).then((res) => {
+      const data = res.data.data.map((d: any) => ({label: d.text, value: String(d.value)}))
+      // .filter((v: any) => !excludeJenisKegiatan.includes(v.label))
+      dispatch(changedValue(ToFieldStateBNV('list_jenis_kegiatan', data)))
+    })
+  }
+
+  const updateJenisUsahaList = () => {
+    axios.get(`http://localhost:3001/jenis-usaha/combobox?$oderby=nama`).then((res) => {
+      const data = res.data.data.map((d: any) => ({label: d.text, value: String(d.value)}))
+      dispatch(changedValue(ToFieldStateBNV('list_jenis_usaha', data)))
+    })
+  }
+
+  const updateJenisPenindakanList = () => {
+    axios.get(`http://localhost:3001/jenis-penindakan/combobox?$oderby=nama`).then((res) => {
+      const data = res.data.data.map((d: any) => ({label: d.text, value: String(d.value)}))
+      dispatch(changedValue(ToFieldStateBNV('list_jenis_penindakan', data)))
+    })
+  }
+
+  useEffect(() => {
+    updateJenisKegiatanList()
+    updateJenisUsahaList()
+    updateJenisPenindakanList()
+  }, [])
+
+  // function onSubmit(e: FormEvent) {
+  //   e.preventDefault()
+  //   console.log('values')
+  //   // console.log(values)
+  //   if (!isLastStep) return next()
+  //   // alert(JSON.stringify(values, null, 2))
+  //   // alert('Your Account Succesfully Created')
+  // }
+
+  const submitPelaporanKegiatan = (values: PelaporanKegiatanState, actions: FormikValues) => {
+    try {
+      if (!isLastStep) {
+        console.log('values')
+        return next()
+      }
+      console.log('laststep', values)
+      alert(JSON.stringify(values, null, 2))
+      actions.setSubmitting(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <Formik
+        validationSchema={currentSchema}
+        initialValues={initialState}
+        onSubmit={submitPelaporanKegiatan}
+      >
+        {({handleReset, handleSubmit, errors, values}) => (
+          <Form className='mx-auto w-100 pt-15 pb-10' id='pelaporan_kegiatan_form'>
+            <div className='card'>
+              <div className='card-body'>
+                {/* <>{(values = {data})}</> */}
+                {step.type.name === 'StepDetailKegiatan' ? (
+                  <StepDetailKegiatan values={values} setVal={setVal} />
+                ) : (
+                  step
+                )}
+                <div className='card mt-5'>
+                  <div className='card-body'>
+                    <div className='row w-100'>
+                      <div className='col'></div>
+                      <div className='col'>
+                        <div className='row d-flex justify-content-end'>
+                          {!isFirstStep && (
+                            <button
+                              type='button'
+                              className='col-5 btn btn-flex btn-secondary px-6 m-3'
+                              onClick={back}
+                            >
+                              ``
+                              <span className='svg-icon svg-icon-2x'>
+                                <i className='fa-solid fa-arrow-left'></i>
+                              </span>
+                              <span className='d-flex flex-column align-items-start ms-2'>
+                                <span className='fs-3 fw-bold'>Kembali</span>
+                              </span>
+                            </button>
+                          )}
+
+                          <button
+                            type='submit'
+                            className='col-5 btn btn-flex btn-primary px-6 m-3'
+                            // onClick={next}
+                          >
+                            <span className='svg-icon svg-icon-2x'>
+                              <i className='fa-solid fa-paper-plane'></i>
+                            </span>
+                            <span className='d-flex flex-column align-items-start ms-2'>
+                              <span className='fs-3 fw-bold'>
+                                {isLastStep ? 'Simpan' : 'Simpan dan Lanjut'}
+                              </span>
+                            </span>
+                          </button>
+                          {/* {!isLastStep ? (
+                            <button
+                              // type='button'
+                              className='col-5 btn btn-flex btn-primary px-6 m-3'
+                              onClick={next}
+                            >
+                              <span className='svg-icon svg-icon-2x'>
+                                <i className='fa-solid fa-paper-plane'></i>
+                              </span>
+                              <span className='d-flex flex-column align-items-start ms-2'>
+                                <span className='fs-3 fw-bold'>Simpan dan Lanjut</span>
+                              </span>
+                            </button>
+                          ) : (
+                            <button
+                              type='submit'
+                              className='col-5 btn btn-flex btn-primary px-6 m-3'
+                            >
+                              <span className='svg-icon svg-icon-2x'>
+                                <i className='fa-solid fa-paper-plane'></i>
+                              </span>
+                              <span className='d-flex flex-column align-items-start ms-2'>
+                                <span className='fs-3 fw-bold'>simpan</span>
+                              </span>
+                            </button>
+                          )} */}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* <button type='submit'>{isLastStep ? 'Simpan' : 'Simpan dan Lanjut'}</button> */}
+              </div>
+            </div>
+
+            {/* <>
+              <div className='card'>
+                <div className='card-body'>
+                  <ul className='nav nav-tabs nav-line-tabs mb-5 fs-6'>
+                    <li className='nav-item'>
+                      <a className='nav-link active' data-bs-toggle='tab' href='#kt_tab_pane_1'>
+                        KEGIATAN
+                      </a>
+                    </li>
+                    <li className='nav-item'>
+                      <a className='nav-link' data-bs-toggle='tab' href='#kt_tab_pane_2'>
+                        TINDAK LANJUT
+                      </a>
+                    </li>
+                    <li className='nav-item'>
+                      <a className='nav-link' data-bs-toggle='tab' href='#kt_tab_pane_3'>
+                        DOKUMENTASI
+                      </a>
+                    </li>
+                  </ul>
+
+                  <div className='tab-content' id='myTabContent'>
+                    <div className='tab-pane fade show active' id='kt_tab_pane_1' role='tabpanel'>
+                      <StepDetailKegiatan values={values} />
+                    </div>
+                    <div className='tab-pane fade' id='kt_tab_pane_2' role='tabpanel'>
+                      <StepTindaklanjut />
+                    </div>
+                    <div className='tab-pane fade' id='kt_tab_pane_3' role='tabpanel'>
+                      <StepDokumentasi />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='card mt-5'>
+                <div className='card-body'>
+                  <div className='row w-100'>
+                    <div className='col'></div>
+                    <div className='col'>
+                      <div className='row'>
+                        <a href='#' className='col-5 btn btn-flex btn-secondary px-6 m-3'>
+                          <span className='svg-icon svg-icon-2x'>
+                            <i className='fa-solid fa-arrow-left'></i>
+                          </span>
+                          <span className='d-flex flex-column align-items-start ms-2'>
+                            <span className='fs-3 fw-bold'>Kembali</span>
+                            <span className='fs-7'>Some description</span>
+                          </span>
+                        </a>
+                        <button type='submit' className='col-5 btn btn-flex btn-primary px-6 m-3'>
+                          <span className='svg-icon svg-icon-2x'>
+                            <i className='fa-solid fa-paper-plane'></i>
+                          </span>
+                          <span className='d-flex flex-column align-items-start ms-2'>
+                            <span className='fs-3 fw-bold'>Simpan</span>
+                            <span className='fs-7'>Some description</span>
+                          </span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </> */}
+          </Form>
+        )}
+      </Formik>
+    </>
+  )
+}
---
+++ src/app/modules/pelaporan/pages/steps/step-tindaklanjut-kejadian.tsx
@@ -0,0 +1,324 @@
+import React, {ChangeEvent, FC} from 'react'
+import Select from 'react-select'
+import {ErrorMessage, Field} from 'formik'
+import {DatePickerField, SelectField, ToFieldStateCE} from '../../components/fields.formikcto'
+import {changedValue} from '../../../../redux/slices/pelaporan-kegiatan.slice'
+import {useDispatch, useSelector} from 'react-redux'
+import {RootState} from '../../../../redux/store'
+
+export const StepTindaklanjutKejadian: FC = () => {
+  const dispatch = useDispatch()
+
+  const listJenisPasal = useSelector((s: RootState) => s.pelaporanKegiatan.list_jenis_pasal)
+  const listJenisPenindakan = useSelector(
+    (s: RootState) => s.pelaporanKegiatan.list_jenis_penindakan
+  )
+  const listJenisPenyelesaian = useSelector(
+    (s: RootState) => s.pelaporanKegiatan.list_jenis_penyelesaian
+  )
+  const listJenisUsaha = useSelector((s: RootState) => s.pelaporanKegiatan.list_jenis_usaha)
+
+  return (
+    <div className='w-100'>
+      <div className='pb-10 pb-lg-15'>
+        <h2 className='fw-bolder text-dark mb-10'>Tindak Lanjut</h2>
+
+        <h3 className='fw-bolder text-dark'>ADMINISTRASI</h3>
+
+        <div className='mb-10 form-group'>
+          <label className='required form-label'>Jenis Pasal</label>
+          <Field
+            name='tindak_lanjut__administrasi__jenis_pasal_selection'
+            target='tindak_lanjut__administrasi__jenis_pasal_id'
+            className='form-control'
+            component={SelectField}
+            options={listJenisPasal}
+            onChange={(o: ChangeEvent<any>) => {
+              dispatch(changedValue(ToFieldStateCE(o)))
+            }}
+          />
+          <div className='text-danger mt-2'>
+            <ErrorMessage name='tindak_lanjut__administrasi__jenis_pasal_id' />
+          </div>
+        </div>
+
+        <div className='mb-10 form-group'>
+          <label className='required form-label'>Jenis Penertiban</label>
+          <Field
+            type='text'
+            name='tindak_lanjut__administrasi__jenis_penertiban'
+            className='form-control'
+            onKeyUp={(o: ChangeEvent<any>) => {
+              dispatch(changedValue(ToFieldStateCE(o)))
+            }}
+          />
+          <div className='text-danger mt-2'>
+            <ErrorMessage name='tindak_lanjut__administrasi__jenis_penertiban' />
+          </div>
+        </div>
+
+        <div className='mb-10 form-group'>
+          <label className='required form-label'>Jenis Pelanggaran</label>
+          <Field
+            as='textarea'
+            type='text'
+            name='tindak_lanjut__administrasi__jenis_pelanggaran'
+            className='form-control'
+            onKeyUp={(o: ChangeEvent<any>) => {
+              dispatch(changedValue(ToFieldStateCE(o)))
+            }}
+          />
+          <div className='text-danger mt-2'>
+            <ErrorMessage name='tindak_lanjut__administrasi__jenis_pelanggaran' />
+          </div>
+        </div>
+
+        <div className='mb-10 form-group'>
+          <label className='required form-label'>PERDA / PERKADA yang dilanggar</label>
+          <Field
+            type='text'
+            name='tindak_lanjut__administrasi__perda_perkada'
+            className='form-control'
+            onKeyUp={(o: ChangeEvent<any>) => {
+              dispatch(changedValue(ToFieldStateCE(o)))
+            }}
+          />
+          <div className='text-danger mt-2'>
+            <ErrorMessage name='tindak_lanjut__administrasi__perda_perkada' />
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col form-group'>
+            <label className='required form-label'>Penyelesaian</label>
+            <Field
+              name='tindak_lanjut__administrasi__penyelesaian_selection'
+              target='tindak_lanjut__administrasi__penyelesaian_id'
+              className='form-control'
+              component={SelectField}
+              options={listJenisPenyelesaian}
+              onChange={(o: ChangeEvent<any>) => {
+                dispatch(changedValue(ToFieldStateCE(o)))
+              }}
+            />
+            <div className='text-danger mt-2'>
+              <ErrorMessage name='tindak_lanjut__administrasi__penyelesaian_id' />
+            </div>
+          </div>
+          <div className='col form-group'>
+            <label className='required form-label'>Penindakan</label>
+            <Field
+              name='tindak_lanjut__jenis_penindakan_selection'
+              target='tindak_lanjut__jenis_penindakan_id'
+              className='form-control'
+              component={SelectField}
+              options={listJenisPenindakan}
+              onChange={(o: ChangeEvent<any>) => {
+                dispatch(changedValue(ToFieldStateCE(o)))
+              }}
+            />
+            <div className='text-danger mt-2'>
+              <ErrorMessage name='tindak_lanjut__jenis_penindakan_id' />
+            </div>
+          </div>
+        </div>
+
+        <div className='row mt-5'>
+          <div className='col'>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <h3 className='fw-bolder text-dark'>IDENTITAS NAMA / USAHA</h3>
+            <div className='form-group'>
+              <label className='required form-label'>Nomor BAP</label>
+              <Field
+                type='text'
+                name='tindak_lanjut__identitas_pelanggar__no_bap'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__identitas_pelanggar__no_bap' />
+              </div>
+            </div>
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Nama/Penanggungjawab</label>
+              <Field
+                type='text'
+                name='tindak_lanjut__identitas_pelanggar__nama_penanggung_jawab'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__identitas_pelanggar__nama_penanggung_jawab' />
+              </div>
+            </div>
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Nama Usaha/Tempat</label>
+              <Field
+                type='text'
+                name='tindak_lanjut__identitas_pelanggar__nama_tempat_usaha'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__identitas_pelanggar__nama_tempat_usaha' />
+              </div>
+            </div>
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Alamat Usaha/Tempat</label>
+              <Field
+                as='textarea'
+                type='text'
+                name='tindak_lanjut__identitas_pelanggar__alamat_tempat_usaha'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__identitas_pelanggar__alamat_tempat_usaha' />
+              </div>
+            </div>
+
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Alamat Pelanggar</label>
+              <Field
+                as='textarea'
+                type='text'
+                name='tindak_lanjut__identitas_pelanggar__alamat'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__identitas_pelanggar__alamat' />
+              </div>
+            </div>
+            <div className='form-group mb-10'>
+              <label className='required form-label'>NIK/Paspport</label>
+              <Field
+                type='text'
+                name='tindak_lanjut__identitas_pelanggar__nik'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__identitas_pelanggar__nik' />
+              </div>
+            </div>
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Jenis Usaha/Tempat</label>
+              <Field
+                name='tindak_lanjut__identitas_pelanggar__jenis_usaha_selection'
+                target='tindak_lanjut__identitas_pelanggar__jenis_usaha_id'
+                className='form-control'
+                component={SelectField}
+                options={listJenisUsaha}
+                onChange={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__identitas_pelanggar__jenis_usaha_id' />
+              </div>
+            </div>
+          </div>
+
+          <div className='col'>
+            <div className='form-group'>
+              <label className='required form-label'>Jumlah Pelanggar</label>
+              <Field
+                type='number'
+                name='tindak_lanjut__jumlah_pelanggar'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__jumlah_pelanggar' />
+              </div>
+            </div>
+
+            <div className='form-group'>
+              <h3 className='fw-bolder text-dark mt-12'>JUMLAH DENDA</h3>
+            </div>
+
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Non Pengadilan</label>
+              <Field
+                type='number'
+                name='tindak_lanjut__denda__non_pengadilan'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__denda__non_pengadilan' />
+              </div>
+            </div>
+
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Tanggal Setor</label>
+              <Field
+                name='tindak_lanjut__denda__tanggal_setor'
+                className='form-control'
+                component={DatePickerField}
+                onChange={(o: any) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__denda__tanggal_setor' />
+              </div>
+            </div>
+
+            <div className='form-group mb-10'>
+              <label className='required form-label'>Nama Bank</label>
+              <Field
+                type='text'
+                name='tindak_lanjut__denda__nama_bank'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__denda__nama_bank' />
+              </div>
+            </div>
+
+            <div className='form-group mb-10'>
+              <label className='required form-label'>No Validasi Bank</label>
+              <Field
+                type='text'
+                name='tindak_lanjut__denda__no_validasi_bank'
+                className='form-control'
+                onKeyUp={(o: ChangeEvent<any>) => {
+                  dispatch(changedValue(ToFieldStateCE(o)))
+                }}
+              />
+              <div className='text-danger mt-2'>
+                <ErrorMessage name='tindak_lanjut__denda__no_validasi_bank' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
