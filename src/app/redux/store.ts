3c83c7f1f8392a2a9f2a3127a19376265867db2d
Azwar Tri Endrayadi <azwartrie123@gmail.com>
2022-12-15 21:30:53 +0700

    add pengawasan & tamu daerah layout

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,10 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import pelaporanKegiatanReducer from "./slices/pelaporan-kegiatan.slice";
+import {configureStore} from '@reduxjs/toolkit'
+import pelaporanKegiatanReducer from './slices/pelaporan-kegiatan.slice'
+import pelaporanKejadianReducer from './slices/pelaporan-kejadian.slice'
+import pelaporanPengawasanReklameReducer from './slices/pelaporan-pengawasan-reklame.slice'
 
 export const store = configureStore({
-    reducer: {
-        pelaporanKegiatan: pelaporanKegiatanReducer
-    },
+  reducer: {
+    pelaporanKegiatan: pelaporanKegiatanReducer,
+    pelaporanKejadian: pelaporanKejadianReducer,
+    pelaporanPengawasan: pelaporanPengawasanReklameReducer,
+    pelaporanTamuDaerah: pelaporanKegiatanReducer,
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
