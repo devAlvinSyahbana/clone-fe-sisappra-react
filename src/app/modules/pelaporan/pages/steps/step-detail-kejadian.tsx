6ed4d85971d42a7fb54a4bccc23e614fc3542565
Azwar Tri Endrayadi <azwartrie123@gmail.com>
2023-01-04 17:12:03 +0700

    add disable dropdown ubah lap kejadian

--- src/app/modules/pelaporan/pages/steps/step-detail-kejadian.tsx
+++ src/app/modules/pelaporan/pages/steps/step-detail-kejadian.tsx
@@ -72,6 +72,7 @@ export const StepDetailKejadian: FC<StepDetailKejadianProps> = ({
             name='kejadian__jenis_kejadian_selection'
             target='kejadian__jenis_kejadian_id'
             className='form-control'
+            disabled={values.id}
             component={SelectField}
             options={jenisKejadianList}
             onChange={(o: ChangeEvent<any>) => {
