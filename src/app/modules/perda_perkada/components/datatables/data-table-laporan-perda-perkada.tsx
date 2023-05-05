c36eb203f2bd41a6c27b08651939a3d18f5596fd
4ryabs <121778412+4ryabs@users.noreply.github.com>
2023-05-05 14:29:56 +0700

    Fixing Data-Table Perda Perkada

--- src/app/modules/perda_perkada/components/datatables/data-table-laporan-perda-perkada.tsx
+++ src/app/modules/perda_perkada/components/datatables/data-table-laporan-perda-perkada.tsx
@@ -1,8 +1,6 @@
 import axios from 'axios'
 import { FC, useState, useEffect } from 'react'
 import DataTable from 'react-data-table-component'
-import { useNavigate } from 'react-router-dom'
-import { string } from 'yup'
 
 export const API_URL = process.env.REACT_APP_SISAPPRA_API_URL
 export const MASTERDATA_URL = process.env.REACT_APP_SISAPPRA_MASTERDATA_API_URL
@@ -24,43 +22,56 @@ const LoadingAnimation = (props: any) => {
 
 export const DtPerdaPerkada: FC<any> = ({
   data,
-  aksi,
   totalRows,
   penertiban,
   handlePageChange,
   handlePerRowsChange,
   wilayahBidang,
-  konfirDel,
   hakAkses,
   loading,
   theme,
 }) => {
 
   const [totalKegiatan, setTotalKegiatan] = useState([])
+  const [totalDendaPengadilan, setTotalDendaPengadilan] = useState([])
+  const [totalDendaNonPengadilan, setTotalDendaNonPengadilan] = useState([])
   useEffect(() => {
-    const fetchDTPimpinan = async () => {
+    const fetchDTPerdaPerkada = async () => {
       const { data } = await axios.get(
         `${PELAPORAN_URL}/kegiatan-umum/?%24top=1&%24select=id%2C%20tindak_lanjut__administrasi__penyelesaian_id%2C%20tindak_lanjut__administrasi__jenis_penertiban`
       )
       const res = await axios.get(
         `${PELAPORAN_URL}/kegiatan-umum/?%24top=${data.total_items}&%24select=id%2C%20tindak_lanjut__administrasi__penyelesaian_id%2C%20tindak_lanjut__administrasi__jenis_penertiban`
       )
+      // const { denda } = await axios.get(
+      //   `${PELAPORAN_URL}/kegiatan-umum/?24top=1&%24select=id%2C%20tindak_lanjut__denda__pengadilan%2C%20tindak_lanjut__administrasi__jenis__penertiban`
+      // )
+      // const resDenda = await axios.get(
+      //   `${PELAPORAN_URL}/kegiatan-umum/?24top=${denda.total_item}&%24select=id%2C%20tindak_lanjut__denda__pengadilan%2C%20tindak_lanjut__administrasi__jenis__penertiban`
+      // )
+      // setTotalDendaPengadilan(resDenda.data.data)
       setTotalKegiatan(res.data.data)
     }
-    fetchDTPimpinan()
+    fetchDTPerdaPerkada()
   }, [])
 
   const GetPerJenis = ({ row, jenis }: any) => {
     let countJumlah = 0
-    if (row === string && !jenis) {
+    // console.log('row', row)
+    // console.log('jenis', jenis)
+    console.log('total',totalKegiatan)
+    if (row && !jenis) {
       countJumlah = totalKegiatan.filter((item: any) => item.tindak_lanjut__administrasi__jenis_penertiban === row).length
     }
     if (row && jenis) {
-      countJumlah = totalKegiatan.filter(
-        (item: any) => item.tindak_lanjut__administrasi__jenis_penertiban === row && item.tindak_lanjut__administrasi__penyelesaian_id === jenis
-      ).length
+      let jumlahJenis = 0
+      for (let i = 0; i < jenis.length; i++) {
+        jumlahJenis = totalKegiatan.filter(
+          (item: any) => item.tindak_lanjut__administrasi__jenis_penertiban === row && item.tindak_lanjut__administrasi__penyelesaian_id === jenis[i]
+        ).length;
+        countJumlah += jumlahJenis
+      }
     }
-    console.log('tes', GetPerJenis)
     return (
       <>
         {countJumlah}
@@ -89,53 +100,54 @@ export const DtPerdaPerkada: FC<any> = ({
       name: 'Jumlah Pelanggaran',
       center: true,
       width: '200px',
-      selector: (row: any) => row.no,
-      cell: (record: any) => <GetPerJenis row={record.no} />,
+      selector: (row: any) => row.jenis_penertiban,
+      cell: (record: any) => <GetPerJenis row={record.jenis_penertiban} />,
     },
     {
       name: 'Peringatan',
       center: true,
       width: '150px',
-      selector: (row: any) => row.no,
-      cell: (record: any) => <GetPerJenis row={record.no} jenis={7} />,
+      selector: (row: any) => row.jenis_penertiban,
+      cell: (record: any) => <GetPerJenis row={record.jenis_penertiban} jenis={[6, 11, 12, 22, 27, 28, 30, 32, 35, 37, 40]} />,
     },
-    {
-      name: 'Penutupan/Penyegelan',
-      center: true,
-      width: '200px',
-      sortField: 'penutupan_penyegelan',
-      selector: (row: any) => row.penutupan_penyegelan,
-    },
-    {
-      name: 'Pencabutan Izin',
-      center: true,
-      width: '150px',
-      sortField: 'pencabutan_izin',
-      selector: (row: any) => row.pencabutan_izin,
-    },
-    {
-      name: 'Yang Lain',
-      center: true,
-      width: '150px',
-      sortField: 'yang_lain',
-      selector: (row: any) => row.yang_lain,
-    },
-    {
-      name: 'Denda Pengadilan',
-      center: true,
-      width: '200px',
-      sortField: 'denda_pengadilan',
-      selector: (row: any) => row.denda_pengadilan,
-    },
-    {
-      name: 'Denda Non Pengadilan',
-      center: true,
-      width: '200px',
-      sortField: 'denda_non_pengadilan',
-      selector: (row: any) => row.denda_non_pengadilan,
-    },
+      {
+        name: 'Penutupan/Penyegelan',
+        center: true,
+        width: '200px',
+        selector: (row: any) => row.jenis_penertiban,
+        cell: (record: any) => <GetPerJenis row={record.jenis_penertiban} jenis={[2, 3, 10, 13, 14, 18, 19, 20, 21, 24]} />,
+      },
+      {
+        name: 'Pencabutan Izin',
+        center: true,
+        width: '150px',
+        selector: (row: any) => row.jenis_penertiban,
+        cell: (record: any) => <GetPerJenis row={record.jenis_penertiban} jenis={[8, 15, 16, 38]} />,
+      },
+      {
+        name: 'Yang Lain',
+        center: true,
+        width: '150px',
+        selector: (row: any) => row.jenis_penertiban,
+        cell: (record: any) => <GetPerJenis row={record.jenis_penertiban} jenis={[1, 4, 5, 7, 9, 17, 23, 25, 26, 29, 32, 33, 34, 36, 39, 41, 42, 43]} />,
+      },
+      {
+        name: 'Denda Pengadilan',
+        center: true,
+        width: '200px',
+        sortField: 'denda_pengadilan',
+        selector: (row: any) => row.denda_pengadilan,
+      },
+      {
+        name: 'Denda Non Pengadilan',
+        center: true,
+        width: '200px',
+        sortField: 'denda_non_pengadilan',
+        selector: (row: any) => row.denda_non_pengadilan,
+      },
   ]
 
+  // console.log('penertiban', penertiban)
   return (
     <div>
       <DataTable
@@ -156,15 +168,51 @@ export const DtPerdaPerkada: FC<any> = ({
 
 export const DtPerdaPerkadaPelaksana: FC<any> = ({
   data,
+  kota,
   totalRows,
   handlePerRowsChange,
   handlePageChange,
   loading,
-  hakAkses,
-  wilayahBidang,
-  konfirDel,
   theme,
 }) => {
+
+  const [totalKegiatan, setTotalKegiatan] = useState([])
+  useEffect(() => {
+    const fetchDTPerdaPerkadaPelaksana = async () => {
+      const { data } = await axios.get(
+        `${PELAPORAN_URL}/kegiatan-umum/?%24top=1&%24select=id%2C%20kegiatan__kota_id%2C%20kegiatan__jenis_kegiatan_id`
+      )
+      const res = await axios.get(
+        `${PELAPORAN_URL}/kegiatan-umum/?%24top=${data.total_items}&%24select=id%2C%20kegiatan__kota_id%2C%20kegiatan__jenis_kegiatan_id`
+      )
+      setTotalKegiatan(res.data.data)
+    }
+    fetchDTPerdaPerkadaPelaksana()
+  }, [])
+
+  const GetPerJenis = ({ row, jenis }: any) => {
+    console.log('row', row)
+    console.log('jenis', jenis)
+    console.log('total',totalKegiatan)
+    let countJumlah = 0
+    if (row && !jenis) {
+      countJumlah = totalKegiatan.filter((item: any) => item.kegiatan__kota_id === row).length
+    }
+    if (row && jenis) {
+      let jumlahJenis = 0
+      for (let i = 0; i < jenis.length; i++) {
+        jumlahJenis = totalKegiatan.filter(
+          (item: any) => item.kegiatan__kota_id === row && item.kegiatan__jenis_kegiatan_id === jenis[i]
+        ).length;
+        countJumlah += jumlahJenis
+      }
+    }
+    return (
+      <>
+        {countJumlah}
+      </>
+    )
+  }
 
   const columns = [
     {
@@ -172,9 +220,9 @@ export const DtPerdaPerkadaPelaksana: FC<any> = ({
       width: '80px',
       sortField: 'id',
       wrap: true,
-      selector: (row: any) => row.no,
+      selector: (row: any) => row.serial,
       cell: (row: any) => {
-        return <div className='mb-2 mt-2'>{row.no}</div>
+        return <div className='mb-2 mt-2'>{row.serial}</div>
       },
     },
     {
@@ -182,53 +230,52 @@ export const DtPerdaPerkadaPelaksana: FC<any> = ({
       wrap: true,
       width: '300px',
       center: true,
-      selector: (row: any) => row.pelaksana,
-      // cell: (record: any) => <GetHakAkses row={parseInt(record.pelaksana)} />,
+      selector: (row: any) => row.pelaksana_kegiatan,
     },
     {
       name: 'Jumlah Pelanggaran',
       center: true,
       width: '200px',
-      sortField: 'jumlah_pelanggaran',
-      selector: (row: any) => row.jumlah_pelanggaran,
+      selector: (row: any) => row.no,
+      cell: (record: any) => <GetPerJenis row={record.no} />,
     },
     {
       name: 'Peringatan',
       center: true,
       width: '150px',
-      sortField: 'peringatan',
-      selector: (row: any) => row.peringatan,
+      selector: (row: any) => row.no,
+      cell: (record: any) => <GetPerJenis row={record.no} jenis={[6, 11, 12, 22, 27, 28, 30, 32, 35, 37, 40]} />,
     },
+      {
+        name: 'Penutupan/Penyegelan',
+        center: true,
+        width: '200px',
+        selector: (row: any) => row.no,
+        cell: (record: any) => <GetPerJenis row={record.no} jenis={[2, 3, 10, 13, 14, 18, 19, 20, 21, 24]} />,
+      },
+      {
+        name: 'Pencabutan Izin',
+        center: true,
+        width: '150px',
+        selector: (row: any) => row.no,
+        cell: (record: any) => <GetPerJenis row={record.no} jenis={[8, 15, 16, 38]} />,
+      },
+      {
+        name: 'Yang Lain',
+        center: true,
+        width: '150px',
+        selector: (row: any) => row.no,
+        cell: (record: any) => <GetPerJenis row={record.no} jenis={[1, 4, 5, 7, 9, 17, 23, 25, 26, 29, 32, 33, 34, 36, 39, 41, 42, 43]} />,
+      },
     {
-      name: 'Penutupan/Penyegelan',
-      center: true,
-      width: '200px',
-      sortField: 'penutupan_penyegelan',
-      selector: (row: any) => row.penutupan_penyegelan,
-    },
-    {
-      name: 'Pencabutan Izin',
-      center: true,
-      width: '150px',
-      sortField: 'pencabutan_izin',
-      selector: (row: any) => row.pencabutan_izin,
-    },
-    {
-      name: 'Yang Lain',
-      center: true,
-      width: '150px',
-      sortField: 'yang_lain',
-      selector: (row: any) => row.yang_lain,
-    },
-    {
-      name: 'Yustisi',
+      name: 'Denda Pengadilan',
       center: true,
       width: '200px',
       sortField: 'denda_pengadilan',
       selector: (row: any) => row.denda_pengadilan,
     },
     {
-      name: 'Non Pengadilan',
+      name: 'Denda Non Pengadilan',
       center: true,
       width: '200px',
       sortField: 'denda_non_pengadilan',
@@ -240,7 +287,7 @@ export const DtPerdaPerkadaPelaksana: FC<any> = ({
     <div>
       <DataTable
         columns={columns}
-        data={data}
+        data={kota}
         progressPending={loading}
         pagination
         paginationServer
